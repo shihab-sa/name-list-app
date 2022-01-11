9eff19b0d69dc9838117dec456f4e0fcf3042763
shihab <game123ahmed@gmail.com>
2022-01-11 21:58:44 +0600

    new app and simple design

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -3,29 +3,63 @@ import data from './data'
 
 const NameList=()=>{
 
-    const [name,setName] = useState(data)
+    const [people,setName] = useState(data)
+
+    const removeItems=(id)=>{
+        let items = people.filter((person)=>person.id !== id)
+        setName(items)
+    }
     return(
-        <div> 
+     
+
+        <div className=' bg-indigo-900 min-h-screen'> 
                {
-                   name.map((person)=>{
+                   people.map((person)=>{
                     const {id,name,age,image} = person
 
                     return(
-                        <div key={id} className='main-control'>
+
+                     
+                        <div key={id} className='main-control flex p-2 justify-center'>
+
+                                 <div className=' bg-gray-200 w-96 p-4 rounded-xl'>
                                   <div className='image-section'>
-                                       <img src={image} alt=''/>
+                                       <img className=' w-11 rounded-full ' src={image} alt=''/>
                                   </div>
-                                  <div>
+                                  <div className='flex justify-center space-x-14'>
                                        <h2>{name}</h2>
-                                       <p>{age}</p>
+                                       <p>Age:{age}</p>
+                                       <div className='  bg-purple-600 px-3 py-1 rounded text-white font-bold' >
+                                       <button onClick={()=>removeItems(id)}>remove</button>
+                                       </div>
+                                       
+
                                        
                                   </div>
-                                 <button onClick={()=>setName([])}>Clear Items</button>
+                                 
+                                  </div>
+
+                                  
+                                  
+                                 
                         </div> 
+                                      
+                        
                     )
                    })
+                                       
                }
+                                       <div className='flex justify-center'>
+                                       <button className=' bg-purple-800 px-4 py-1 rounded text-white font-bold' onClick={()=>setName([])}>Clear Items</button>
+                                       </div> 
+                                      
+             
+                                                        
+                                       
         </div>
+                                       
+
+        
     )
 }
 
